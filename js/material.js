c122562b6aa57dfd989da12050ad3b3f6a066d7c
Anthony-713 <joanzavalavallejo@gmail.com>
2023-01-27 13:40:40 -0600

    modificado

--- js/material.js
+++ js/material.js
@@ -1,10 +1,10 @@
 (function(){
-    const material = [...document.querySelectorAll('.material_ar')];
-    material.forEach(M =>[
-        M.addEventListener('click', ()=>{
+    const material = [...document.querySelectorAll('.MTitle')];
+    material.forEach(materiales =>[
+        materiales.addEventListener('click', ()=>{
             let height = 0;
-            let answer = question.nextElementSibling;
-            let addPadding = question.parentElement.parentElement;
+            let answer = materiales.nextElementSibling;
+            let addPadding = materiales.parentElement.parentElement;
             
             addPadding.classList.toggle('material_ar--add')
             if(answer.clientHeight === 0){
@@ -13,5 +13,24 @@
 
             answer.style.height = `${height}px`;
         })
-    ])
+    ]);
+    const selector = document.querySelector('#TSelect');
+    const opciones = document.querySelector('#opciones');
+    const contenidoselect = document.querySelector('#TSelect .tscontenido');
+    const hiddenInput = document.querySelector('#inputselect');
+    
+    document.querySelectorAll('#opciones > .opcion').forEach((opcion) => {
+        opcion.addEventListener('click', (e)=>{
+            e.preventDefault();
+            contenidoselect.innerHTML = e.currentTarget.innerHTML;
+            selector.classList.toggle('active');
+            opciones.classList.toggle('active');
+            hiddenInput.value = e.currentTarget.querySelector('.tamaños').innerText;
+        });
+    });
+    selector.addEventListener('click', ()=>{
+        selector.classList.toggle('active');
+        opciones.classList.toggle('active');
+    });
+    
 })();
